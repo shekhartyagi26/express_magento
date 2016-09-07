e51fa35f649b378c3cbab2d7ccd44270e0cb3f13
shekhar <shekhar@excellencetechnologies.in>
2016-09-07 17:37:30 +0530

    registration api

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 jsonData = [
 	{"email": "manish@excellencetechnologies.in","password": "java@123","url":"/customer/login/"},
  {'APP_ID': 'com.tethr'},
+ {"firstname": "shekhar","lastname": "tyagi","email":"shekhar13@gmail.com","password":"123456","url":"/customer/register/"},
 ]
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ require('node-import');
 imports('config/index');
 
 
-router.all('/', function (req, res) {
+router.all('/login', function (req, res) {
 
     if (jsonData[0].email.length > 0 && jsonData[0].password.length > 0) {
         request({
@@ -30,4 +30,32 @@ router.all('/', function (req, res) {
         });
     }
 });
+
+
+router.all('/register', function (req, res) {
+    if (jsonData[2].firstname.length > 0 && jsonData[2].lastname.length > 0 && jsonData[2].email.length > 0 && jsonData[2].password.length > 0) {
+        request({
+            url: 'http://144.76.34.244:8080/magento/1.9/web/index.php/excellence/mobile/api/v1' + jsonData[2].url, //URL to hit
+            method: 'GET',
+            headers: jsonData[1],
+            body: JSON.stringify({
+                firstname: jsonData[2].firstname,
+                lastname: jsonData[2].lastname,
+                email: jsonData[2].email,
+                password: jsonData[2].password
+            })
+
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+                res.json(error);
+            } else {
+                console.log(response.statusCode, body);
+                res.json(response.statusCode, body);
+            }
+        });
+    }
+
+});
+
 module.exports = router;
