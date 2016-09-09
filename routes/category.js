74373fa6d9cbaee0796e3bb347fef36a377ae119
shekhar <shekhar@excellencetechnologies.in>
2016-09-09 12:41:54 +0530

    implement_redis_in_categoryProducts_api

--- routes/category.js
+++ routes/category.js
@@ -5,34 +5,58 @@ var request = require('request');
 require('node-import');
 imports('config/index');
 
+var redis = require("redis"),
+        client = redis.createClient();
+
 
 router.post('/products', function (req, res) {
     var id = req.body.id;
-    if (id.length > 0) {
-        request({
-            url: jsonData[1].url + '/category/products/', //URL to hit
-            method: 'post',
-            headers: jsonData[0],
-            body: JSON.stringify({
-                id: id,
-            })
-        }, function (error, response, body) {
-            if (error) {
-                console.log(error);
-                res.json(error);
-            }
-            if (response.statusCode == 500) {
-                console.log("not found");
-                res.json({status: 0, error: response.statusCode, msg: "not found"});
+
+    if (id > 0) {
+        client.hgetall('category', function (err, object) {
+            if (object.id == id) {
+                console.log("exist");
+                res.json(object);
+
             } else {
+                console.log('doesnt exist');
+                res.json('doesnt exist');
+                request({
+                    url: jsonData[1].url + '/category/products/', //URL to hit
+                    method: 'post',
+                    headers: jsonData[0],
+                    body: JSON.stringify({
+                        id: id
+                    })
 
-                console.log(response.statusCode, body);
+                }, function (error, response, body) {
+                    if (error) {
+                        console.log(error);
+                        res.json(error);
+                    }
+                    if (response.statusCode == 500) {
+                        console.log("not found");
+                        res.json({status: 0, error: response.statusCode, msg: "not found"});
+                    } else {
 
-                res.json({status: 1, statuscode: response.statusCode, body: body});
+                        client.hmset('category', {
+                            'id': id,
+                            "body": body
+                        });
+                        client.hgetall('category', function (err, object) {
+                            console.log(object);
+                        });
+                        console.log(response.statusCode, body);
+                        res.json({status: 1, statuscode: response.statusCode, body: body});
+                    }
+                });
             }
         });
     } else {
         res.json({status: 0, error: "500", msg: "Invalid Fields"});
+        console.log("Invalid Fields")
     }
 });
+
+
 module.exports = router;
--- routes/customer.js
+++ routes/customer.js
@@ -4,73 +4,39 @@ var path = require('path');
 var request = require('request');
 require('node-import');
 imports('config/index');
-var redis = require("redis"),
-        client = redis.createClient();
 
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
-client.on('connect', function () {
-    console.log('connected');
-});
 
 router.post('/login', function (req, res) {
     var email = req.body.email;
     var password = req.body.password;
     if (email.length > 0 && password.length > 0) {
-        client.hgetall('frame', function (err, object) {
-            if (object.email === email && object.password === password) {
-                console.log("exist");
-                console.log(object.email);
-                console.log(object.password);
-                console.log(object);
-                res.json(object);
+        request({
+            url: jsonData[1].url + '/customer/login/', //URL to hit
+            method: 'post',
+            headers: jsonData[0],
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
 
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+                res.json(error);
+            }
+            if (response.statusCode == 500) {
+                console.log("not found");
+                res.json({status: 0, error: response.statusCode, msg: "not found"});
             } else {
-                console.log('doesnt exist');
-                res.json('doesnt exist');
-                request({
-                    url: jsonData[1].url + '/customer/login/', //URL to hit
-                    method: 'post',
-                    headers: jsonData[0],
-                    body: JSON.stringify({
-                        email: email,
-                        password: password
-                    })
 
-                }, function (error, response, body) {
-                    if (error) {
-                        console.log(error);
-                        res.json(error);
-                    }
-                    if (response.statusCode == 500) {
-                        console.log("not found");
-                        res.json({status: 0, error: response.statusCode, msg: "not found"});
-                    } else {
-
-                        client.hmset('frame', {
-                            'email': email,
-                            'password': password,
-                            "body": body
-                        });
-                        client.hgetall('frame', function (err, object) {
-                            console.log(object);
-                        });
-                        console.log(response.statusCode, body);
-                        res.json({status: 1, statuscode: response.statusCode, body: body});
-                    }
-                });
-
-
+                console.log(response.statusCode, body);
+                res.json({status: 1, statuscode: response.statusCode, body: body});
             }
         });
     } else {
         res.json({status: 0, error: "500", msg: "Invalid Fields"});
-        console.log("Invalid Fields")
     }
 });
-
-
 
 
 router.post('/register', function (req, res) {
