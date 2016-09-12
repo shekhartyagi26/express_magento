2cddc7c6cd96752e221a02b114502678e4c0fe55
shekhar <shekhar@excellencetechnologies.in>
2016-09-12 13:19:53 +0530

    category.js

--- routes/category.js
+++ routes/category.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 var path = require('path');
@@ -8,18 +9,16 @@ imports('config/index');
 var redis = require("redis"),
         client = redis.createClient();
 
-
 router.post('/products', function (req, res) {
     var id = req.body.id;
-
     if (id > 0) {
-        client.hgetall('category', function (err, object) {
-            if (object.id == id) {
+        client.hgetall('category_'+id, function (err, object) {
+            if (object!= null && object.id == id ) {
                 console.log("exist");
-                console.log(object);
-                res.json({msg: "exist", statuscode: "200", data: object});
+                console.log(object.id);
+                 res.json({msg: "exist", statuscode: "200", data: object});
 
-            } else {
+            } else  {
                 request({
                     url: jsonData[1].url + '/category/products/', //URL to hit
                     method: 'post',
@@ -35,16 +34,15 @@ router.post('/products', function (req, res) {
                     }
                     if (response.statusCode == 500) {
                         console.log("not found");
-                        res.json({status: 0, error: response.statusCode, msg: "not found"});
+                         res.json({status: 0, error: response.statusCode, msg: "not found"});
                     } else {
 
-                        client.hmset('category', {
+                        client.hmset('category_'+id, {
                             'id': id,
                             "body": body
                         });
-                        // client.hgetall('category', function (err, object) {
-                        //     console.log(object);
-                        // });
+                       
+                        console.log("doesnt exist");
                         console.log(response.statusCode, body);
                         res.json({msg: "doesnt exist", statuscode: response.statusCode, body: body});
                     }
@@ -52,7 +50,7 @@ router.post('/products', function (req, res) {
             }
         });
     } else {
-        res.json({status: 0, error: "500", msg: "Invalid Fields"});
+         res.json({status: 0, error: "500", msg: "Invalid Fields"});
         console.log("Invalid Fields");
     }
 });
