9e590d59baad56317e2a713470578e22abe98a96
shekhar <shekhar@excellencetechnologies.in>
2016-09-26 13:05:45 +0530

    apply only in account.js

--- routes/account.js
+++ routes/account.js
@@ -15,28 +15,26 @@ const request_ = require('../service/request');
 router.all('/address', function (req, res) {
     var secret = req.body.secret;
     var access_token = req.headers.authorization;
-var headers = req.headers.headers;
-var verify = req.Collection;
-request_.headerVerify(headers ,verify, function(headers, url,msg){
-    if (secret.length > 0 && headers.length > 0 && url.length > 0) {
-        var body = ({secret: secret});
-        var headers_ =  {APP_ID: config.APP_ID, "Authorization": access_token};
-        var url_ = url+'/account/address/';
-        console.log(headers_);
-        console.log(url_);
-        request_.request(body, headers_, url_, function (req, response, msg) {
-            if (msg == ERROR) {
-                res.json({status: 0, statuscode: ERR_STATUS, error: response});
-            } else if (req.statusCode == ERR_STATUS) {
-                res.json({status: 0, statuscode: req.statusCode, body: response});
-            } else {
-                res.json({status: 1, statuscode: req.statusCode, body: response});
-                console.log(response);
-            }
-        });
-    } else {
-        res.json({status: 0, statuscode: ERR_STATUS, msg: INVALID});
-    }
+    var headers = req.headers.headers;
+    var verify = req.Collection;
+    request_.headerVerify(headers, verify, function (headers, url, msg) {
+        if (secret.length > 0 && headers.length > 0 && url.length > 0) {
+            var body = ({secret: secret});
+            var headers_ = {APP_ID: config.APP_ID, "Authorization": access_token};
+            var url_ = url + '/account/address/';
+            request_.request(body, headers_, url_, function (req, response, msg) {
+                if (msg == ERROR) {
+                    res.json({status: 0, statuscode: ERR_STATUS, error: response});
+                } else if (req.statusCode == ERR_STATUS) {
+                    res.json({status: 0, statuscode: req.statusCode, body: response});
+                } else {
+                    res.json({status: 1, statuscode: req.statusCode, body: response});
+                    console.log(response);
+                }
+            });
+        } else {
+            res.json({status: 0, statuscode: ERR_STATUS, msg: INVALID});
+        }
     });
 });
 
--- service/request.js
+++ service/request.js
@@ -5,7 +5,7 @@ imports('config/constant');
 
 exports.request = function (body, headers, url_, callback) {
     request({
-        url:  url_, //URL to hit
+        url: url_, //URL to hit
         method: 'post',
         headers: headers,
         timeout: 10000,
@@ -19,19 +19,19 @@ exports.request = function (body, headers, url_, callback) {
             callback(result, body, SUCCESS);
         }
     });
-} 
-exports.headerVerify = function ( headers,verify, callback) {
-     
+}
+exports.headerVerify = function (headers, verify, callback) {
+
     if (headers.length > 0) {
-        verify.findOne({headers: headers}, function (error,user,body) {
+        verify.findOne({headers: headers}, function (error, user, body) {
             if (error) {
                 callback(500, error, ERROR);
             }
             if (!user) {
                 callback(user, body, NOTFOUND)
-            }   
-               var url= JSON.parse(JSON.stringify(user)).URL;
-             callback(headers , url, SUCCESS);
+            }
+            var url = JSON.parse(JSON.stringify(user)).URL;
+            callback(headers, url, SUCCESS);
 
         })
     } else {
