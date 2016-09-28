18421964a7fd857677e303cb4fa102642b6da45d
shekhar <shekhar@excellencetechnologies.in>
2016-09-28 10:39:53 +0530

    changes in comments

--- middleware/verify.js
+++ middleware/verify.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
+var _ = require('lodash');
 var Schema = mongoose.Schema;
 var verifySchema = new Schema({
     headers: String,
@@ -14,11 +15,11 @@ module.exports = function (req, res, next) {
     var dtabase = req.app;
     var promise = dtabase.findOne({APP_ID: headers}).exec();
     promise.then(function (verify) {
-        verify.url = JSON.parse(JSON.stringify(verify)).URL;
-        return verify.save(); // returns a promise
+        url = JSON.parse(JSON.stringify(verify)).URL;
+        return verify.url; // returns a promise
     })
             .then(function (verify) {
-                var URL = verify.url;
+                var URL = url;
                 req.URL = URL;
                 next();
             })
