7e7c8d659af9ea2f5da405ec7d669713c58b41d5
shekhar <shekhar@excellencetechnologies.in>
2016-10-05 17:58:24 +0530

    redis data is dynamically

--- middleware/redis.js
+++ middleware/redis.js
@@ -9,26 +9,13 @@ module.exports = function (req, res, next) {
     var promise = dtabase.findOne({APP_ID: headers}).exec();
     if (headers.length > 0) {
         promise.then(function (redis) {
-            header = redis.get('APP_ID');
-            if (header == "com.tethr") {
-                client.select(1, function (err, res) {
-                    client.set('key', 'string'); // this will be posted to database 1 rather than db 0
-                    next();
-                });
-            } else if (headers == 'com.react.tethr') {
-                client.select(2, function (err, res) {
-                    client.set('key', 'string'); // this will be posted to database 1 rather than db 0
-                    next();
-                });
-            } else {
-                client.select(3, function (err, res) {
-                    client.set('key', 'string'); // this will be posted to database 1 rather than db 0
-                    next();
-                });
-            }
+            HeaderId = redis.get('HeaderId');
+            client.select(HeaderId, function (err, res) {
+                client.set('key', 'string');
+                next();
+            });
         })
                 .catch(function (err) {
-                    console.log(err);
                     res.json({status: 0, statuscode: 500, body: "header is not found in database"});
                 });
     } else if (headers == undefined) {
