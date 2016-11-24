2bddf0006d101b6b9c2ccca46a17f80d5eec5d1c
manishiitg <manishiitg>
2016-11-24 13:21:54 +0530

    Fixed the comments.

--- middleware/verify.js
+++ middleware/verify.js
@@ -4,22 +4,22 @@ var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 module.exports = function (req, res, next) {
-    var headers = req.headers.app_id;
+    var appId = req.headers.app_id;
     var dtabase = req.app;
     var promise = dtabase.findOne({
-        APP_ID: headers
+        APP_ID: appId
     }).exec();
-    if (headers.length > 0) {
+    if (appId.length > 0) {
         promise.then(function (verify) {
             URL = verify.get('URL');
             req.URL = URL;
             next();
         }).catch(function (err) {
-            res.json({status: 0, statuscode: 500, body: "URL is not found in database"});
+            res.json({status: 0, statuscode: 500, body: "APP ID Not Found in Database!"});
         });
-    } else if (headers == undefined) {
-        res.json({status: 0, statuscode: 500, body: "header is undefined"});
+    } else if (appId == undefined) {
+        res.json({status: 0, statuscode: 500, body: "Unexpected Error!"});
     } else {
-        res.json({status: 0, statuscode: 500, body: "header cannot be empty"});
+        res.json({status: 0, statuscode: 500, body: "APP ID Cannot Be Empty!"});
     }
 };
