11fdd87504d740a108837b46b9d4df9a127d73e6
shekhar <shekhar@excellencetechnologies.in>
2016-11-30 12:47:22 +0530

    url changed, also create png image as well wabp image

--- config/index.js
+++ config/index.js
@@ -1,1 +1,9 @@
-config = {'APP_ID': 'com.tethr', "DbUrl":"mongodb://127.0.0.1/magento_mobile", "URL": "http://144.76.34.244:8080/magento/mobile/web/index.php/excellence/mobile/api/v1", "CATEGORY_EXPIRESAT": "216000", "PRODUCT_EXPIRESAT": "216000", "ACCOUNT_EXPIRESAT":"21600", "IMAGE_URL" :"http://144.76.34.244:8080/shekhar_works/Eexpress_magento/public/", "IMAGE_MINIFY_URL" :"http://144.76.34.244:8080/shekhar_works/Eexpress_magento/public/minify/", "DEFAULT_IMAGE_URL":"http://144.76.34.244:8080/shekhar_works/Eexpress_magento/public/default/default.webp"}
+config = {'APP_ID': 'com.tethr',
+    "DbUrl": "mongodb://127.0.0.1/magento_mobile",
+    "URL": "http://144.76.34.244:8080/magento/mobile/web/index.php/excellence/mobile/api/v1",
+    "CATEGORY_EXPIRESAT": "216000",
+    "PRODUCT_EXPIRESAT": "216000",
+    "ACCOUNT_EXPIRESAT": "21600",
+    "IMAGE_URL": "http://d3r38l4d4yyrur.cloudfront.net/",
+    "DEFAULT_IMAGE_URL": "http://d3r38l4d4yyrur.cloudfront.net//default/default.webp"
+};
