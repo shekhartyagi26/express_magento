11a9f9d05cef28fea4d6780c3ab446c1f939170c
shekhar <shekhar@excellencetechnologies.in>
2016-12-01 12:17:13 +0530

    before taking pull of branch cron

--- routes/cron.js
+++ routes/cron.js
@@ -3,18 +3,19 @@ var router = express.Router();
 var CronJob = require('cron').CronJob;
 var moment = require('moment');
 
+var IST_timezone = moment().tz("Asia/Kolkata").format('h:mm:ss a');
+var EST_timezone = moment().tz("Europe/London").format('h:mm:ss a');
+var PST_timezone = moment().tz("America/Los_Angeles").format('h:mm:ss a');
+
 router.post('/db', function (req, res) {
-		Crone_running_time =	req.Crone_running_time;
+    Crone_running_time = req.Crone_running_time;
 
 // // pattern for crone wor aafter 5 min '*/5 * * * *'
-new CronJob('* * * * * *', function() {
-  console.log('You will see this message every second');
-		
-	// if(moment().format('h:mm:ss') == Crone_running_time ){
- //  console.log(moment().format('h:mm:ss a'));
-	// }
-	// console.log(moment().tz("Asia/Calcutta|Asia/Kolkata").format());
-
-}, null, true, 'America/Los_Angeles');
+    new CronJob('* * * * * *', function () {
+        console.log('You will see this message every second');
+        if (moment().format('h:mm:ss a') > '11:43:22 am') {
+            console.log(moment().format('h:mm:ss a'));
+        }
+    }, null, true, 'America/Los_Angeles');
 });
 module.exports = router;
