99c480d7cd897a366ebc2e7ddcfdbb4a5f5cea57
shekhar <shekhar@excellencetechnologies.in>
2016-12-06 17:56:00 +0530

    image url

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ var config = {
     "PRODUCT_EXPIRESAT": "216000",
     "ACCOUNT_EXPIRESAT": "21600",
     // "CDN_URL": "http://d3r38l4d4yyrur.cloudfront.net/",
-        "CDN_URL": "http://d3r38l4d4yyrur.cloudfront.net/",
+        "CDN_URL": "http://144.76.34.244:7012/",
     "DEFAULT_IMAGE_URL": "http://d3r38l4d4yyrur.cloudfront.net/default/default.webp",
     "IMAGE_URL": "http://144.76.34.244:8080/shekhar_works/expressmagento_live/express_magento/public/",
     "IMAGE_MINIFY_URL": "http://144.76.34.244:8080/shekhar_works/expressmagento_live/express_magento/public/minify/"
